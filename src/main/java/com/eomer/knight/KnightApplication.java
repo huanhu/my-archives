c902adbcd197e959bfac50d80762f9d526a70c99
knight <593787915@qq.com>
2019-12-19 11:07:43 +0800

    不同模块不同级别日志输出

--- src/main/java/com/eomer/knight/KnightApplication.java
+++ src/main/java/com/eomer/knight/KnightApplication.java
@@ -15,8 +15,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2// 启动swagger ui注解
 public class KnightApplication {
 
-    private static Logger logger = LoggerFactory.getLogger("test1Logger");
-    private static Logger logger2 = LoggerFactory.getLogger("test2Logger");
+    private static Logger logger = LoggerFactory.getLogger("SysLogger");
+    private static Logger logger2 = LoggerFactory.getLogger("ApiLogger");
     private static Logger logger3 = LoggerFactory.getLogger(KnightApplication.class);
     //private static Logger logger4 = LoggerFactory.getLogger("TmpLog");
     /** 
@@ -27,13 +27,13 @@ public class KnightApplication {
     * @date 2019/10/8 11:11
     */ 
     public static void main(String[] args) {
-        logger.info("slf4j 启动 test1Logger info ");
-        logger.warn("slf4j 启动 test1Logger warn ");
-        logger.error("slf4j 启动 test1Logger error ");
+        logger.info("slf4j 启动 SysLogger info ");
+        logger.warn("slf4j 启动 SysLogger warn ");
+        logger.error("slf4j 启动 SysLogger error ");
 
-        logger2.info("slf4j 启动 test2Logger info ");
-        logger2.warn("slf4j 启动 test2Logger warn ");
-        logger2.error("slf4j 启动 test2Logger error ");
+        logger2.info("slf4j 启动 ApiLogger info ");
+        logger2.warn("slf4j 启动 ApiLogger warn ");
+        logger2.error("slf4j 启动 ApiLogger error ");
 
         logger3.info("slf4j 启动 默认 info ");
         logger3.warn("slf4j 启动 默认 warn ");
